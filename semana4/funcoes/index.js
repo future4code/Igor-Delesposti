c1cd05ac0968d1492a0d6559cf8861bea04bd3fb
igordelesposti <igordelesposti@gmail.com>
2020-03-09 18:13:11 -0300

    Entrega final com todos exercícios feitos

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -80,32 +80,86 @@
 
 //5)
 
-let seculo = (ano) => {
-    if(ano >= 1001 && ano <= 1100){
-      return "O " + ano + " pertence ao século XI";
-  }else if(ano >= 1101 && ano <= 1200){
-      return "O " + ano + " pertence ao século XII";
-  }else if(ano >= 1201 && ano <= 1300){
-      return "O " + ano + " pertence ao século XIII";
-  }else if(ano >= 1301 && ano <= 1400){
-      return "O " + ano + " pertence ao século XIV";
-  }else if(ano >= 1401 && ano <= 1500){
-      return "O " + ano + " pertence ao século XV";
-  }else if(ano >= 1501 && ano <= 1600){
-      return "O " + ano + " pertence ao século XVI";
-  }else if(ano >= 1601 && ano <= 1700){
-      return "O " + ano + " pertence ao século XVII";
-  }else if(ano >= 1701 && ano <= 1800){
-      return "O " + ano + " pertence ao século XVIII";
-  }else if(ano >= 1801 && ano <= 1900){
-      return "O " + ano + " pertence ao século XIX";
-  }else if(ano >= 1901 && ano <= 2000){
-      return "O " + ano + " pertence ao século XX";
-  }else if(ano >= 2001 && ano <= 2020){
-      return "O " + ano + " pertence ao século XXI";
-  }else{
-      return "Ano fora do intervalo entre 1000dc e 2020dc.";
-  }
-  }
+// let seculo = (ano) => {
+//     if(ano >= 1001 && ano <= 1100){
+//       return "O " + ano + " pertence ao século XI";
+//   }else if(ano >= 1101 && ano <= 1200){
+//       return "O " + ano + " pertence ao século XII";
+//   }else if(ano >= 1201 && ano <= 1300){
+//       return "O " + ano + " pertence ao século XIII";
+//   }else if(ano >= 1301 && ano <= 1400){
+//       return "O " + ano + " pertence ao século XIV";
+//   }else if(ano >= 1401 && ano <= 1500){
+//       return "O " + ano + " pertence ao século XV";
+//   }else if(ano >= 1501 && ano <= 1600){
+//       return "O " + ano + " pertence ao século XVI";
+//   }else if(ano >= 1601 && ano <= 1700){
+//       return "O " + ano + " pertence ao século XVII";
+//   }else if(ano >= 1701 && ano <= 1800){
+//       return "O " + ano + " pertence ao século XVIII";
+//   }else if(ano >= 1801 && ano <= 1900){
+//       return "O " + ano + " pertence ao século XIX";
+//   }else if(ano >= 1901 && ano <= 2000){
+//       return "O " + ano + " pertence ao século XX";
+//   }else if(ano >= 2001 && ano <= 2020){
+//       return "O " + ano + " pertence ao século XXI";
+//   }else{
+//       return "Ano fora do intervalo entre 1000dc e 2020dc.";
+//   }
+//   }
   
-  console.log(seculo(1800));
+//   console.log(seculo(1800));
+
+//6
+//a
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+const tamanhoArray = (array) => {
+    return array.length
+}
+console.log(tamanhoArray(array))
+
+//b
+const booleano = (numero) =>{
+    if(numero % 2 === 0){
+        return  true
+    }else{
+        return  false
+    }
+}
+
+console.log(booleano(55))
+
+//c
+const quantidadePar = (array) => {
+    let arrayPar = 0
+    for(let elemento of array){
+        if(elemento % 2 === 0){
+            arrayPar++
+        }
+    }
+    return arrayPar
+}
+console.log(quantidadePar(array))
+
+//d
+const quantidadePar1 = (array) => {
+    let arrayPar = 0
+    for(let elemento of array){
+        if(booleano(elemento) === true){
+            arrayPar++
+        }
+        
+    }
+    return arrayPar
+}
+console.log(quantidadePar1(array))
+
+
+
+
+
+
+
+
+
+
