c27ba76e21aba8c6ad6d5bb2788d64ff902c21de
igordelesposti <igordelesposti@gmail.com>
2020-03-13 11:15:29 -0300

    Exercicio 2 - interpretação

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,2 +1,11 @@
 //------Exercício de interpretação de código------
-//Exercício 1.
+//Exercício 1. É uma função de conversão do Dólar em Real. O usuário alimenta a variável cotacao e a função retorna
+//esse valor em Reais através de valorEmDolar * cotacao. A variável meuDinheiro armazena esse resultado e imprime no console
+//o resultado da conversão.
+
+//Exercício 2. A função investeDinheiro recebe os parâmetros(tipoDeInvestimento e valor).
+//A variável vazia valorAposInvestimento vai ser alimentada com o output da função.
+//Depois ocorre a verificação com o switch/case, em que se o primeiro parâmetro da função
+//for um dos "case", ele alimenta a variável valorAposInvestimento com o segundo parâmetro da função e realiza multiplicação, de acordo com o case selecionado.
+// As variáveis novoMontante e segundoMontante são apenas para exemplo da invocação da função alimentando o case "Ações" e valor 150.
+//No final o código imprime no console o valor das variáveis novoMontante e segundoMontante que foram invocadas com os parâmetros necessários.
