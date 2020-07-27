0614fc72b8eab2c64782e7ee32b861b78a044b96
igordelesposti <igordelesposti@gmail.com>
2020-07-27 18:29:50 -0300

    Entrega do exercicio da tarde

---
+++ semana21/resolvendo-problemas/ex1.ts
@@ -0,0 +1,24 @@
+export const isOneEdit = (
+    source: string,
+    comparision: string
+  ): boolean => {
+    if (
+      comparision.length > source.length + 1 ||
+      comparision.length < source.length - 1
+    ) {
+      return false;
+    }
+    let communCharQuantity = 0;
+  
+    for (const char of comparision) {
+      if (source.indexOf(char) !== -1) {
+        communCharQuantity++;
+      }
+    }
+    return (
+      communCharQuantity <= source.length + 1 &&
+      communCharQuantity >= source.length - 1
+    );
+  };
+
+  console.log(isOneEdit("bananaaa", "banana"))
